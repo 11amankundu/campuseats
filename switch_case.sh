b9d4dafc8f604195bf0d95132c7e66ebf33ee7a1
sanjog181002-commits <sanjog181002@gmail.com>
2026-08-26 22:51:23 +0530

    Add files via upload

---
+++ switch_case.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Enter month number (1-12):"
+read month_num
+
+case $month_num in
+    1) echo "January" ;;
+    2) echo "February" ;;
+    3) echo "March" ;;
+    4) echo "April" ;;
+    5) echo "May" ;;
+    6) echo "June" ;;
+    7) echo "July" ;;
+    8) echo "August" ;;
+    9) echo "September" ;;
+    10) echo "October" ;;
+    11) echo "November" ;;
+    12) echo "December" ;;
+    *) echo "Invalid month number" ;;
+esac
